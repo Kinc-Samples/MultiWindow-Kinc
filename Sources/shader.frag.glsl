b5ab757f1771cb59ac813375aeb24b9b877f3a90
Robert Konrad <git2016@robdangero.us>
2022-06-03 19:14:23 +0200

    Port things to C

--- Sources/shader.frag.glsl
+++
@@ -1,8 +0,0 @@
-#version 450
-
-uniform vec3 color;
-out vec4 FragColor;
-
-void main() {
-	FragColor = vec4(color.rgb, 1.0);
-}
--- korefile.js
+++
@@ -1,6 +0,0 @@
-let project = new Project('MultiWindow');
-
-project.addFile('Sources/**');
-project.setDebugDir('Deployment');
-
-resolve(project);
